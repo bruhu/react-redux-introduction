9cb49ecc787bd8eaae89656afd00303e59b175fc
Bru <annabru.abh@gmail.com>
2019-05-29 13:50:15 +0200

    Header component now renders from index.js

--- react-todo-list/src/index.js
+++ react-todo-list/src/index.js
@@ -1,11 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
+import Header from "./components/Header";
 
-class Header extends React.Component {
-    render(){
-        return <h1>What to do?</h1>
-    }
-}
-
-render(<Header/>, document.querySelector('#main'))
+render(<Header />, document.querySelector("#main"));
 // render(<p>Hello, World!</p>, document.querySelector("#main"));
