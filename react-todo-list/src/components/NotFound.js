9066a22f37eb9326d4b269eb2aadec4921e6a41b
Bru <annabru.abh@gmail.com>
2019-05-29 15:40:57 +0200

    Typo

--- react-todo-list/src/components/NotFound.js
+++ react-todo-list/src/components/NotFound.js
@@ -1,24 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import Header from "./Header";
 
-export class NotFound extends Component {
-    componentDidMount() {
-        document.title = "Error 404 - What to do?"
-    }
-    render() {
-        return (
-            <div className="container">
-                <Header tagline="404 - Page not found!">
-                    <div className="alert alert-warning">
-                        <strong>
-                            Oops... sorry!<br></br>
-                        </strong>
-                        The requested page could not be found.
-                    </div>
-                </Header>
-            </div>
-        )
-    }
+class NotFound extends Component {
+  componentDidMount() {
+    document.title = "Error 404 - What to do?";
+  }
+  render() {
+    return (
+      <div className="container">
+        <Header tagline="404 - Page not found!">
+          <div className="alert alert-warning">
+            <strong>
+              Oops... sorry!<br />
+            </strong>
+            The requested page could not be found.
+          </div>
+        </Header>
+      </div>
+    );
+  }
 }
 
-export default NotFound
+export default NotFound;
