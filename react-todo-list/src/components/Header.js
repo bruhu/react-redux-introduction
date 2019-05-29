40037b40e7183e59ad3982fd7bf701dc9dc23b64
Bru <annabru.abh@gmail.com>
2019-05-29 15:18:44 +0200

    Header is now a functional component

--- react-todo-list/src/components/Header.js
+++ react-todo-list/src/components/Header.js
@@ -1,14 +1,25 @@
 import React from "react";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header className="header">
+// class Header extends React.Component {
+//   render() {
+//     return (
+//       <header className="header">
+//         <h1>What to do?</h1>
+//         <span className="tagline">{this.props.tagline}</span>
+//       </header>
+//     );
+//   }
+// }
+
+//same as above, but turning it into a stateless functional component
+const Header = props => {
+    return(
+        <header className="header">
         <h1>What to do?</h1>
-        <span className="tagline">{this.props.tagline}</span>
+        <span className="tagline">{props.tagline}</span>
       </header>
-    );
-  }
+    )
 }
 
 export default Header;
+
---
+++ react-todo-list/src/components/Help.js
@@ -0,0 +1,10 @@
+// template for a manual that helps users to use the application
+
+import React from "react"
+import Header from "./Header"
+
+class Help extends React.Component {
+    componentDidMount(){
+        document.title = "Help - what to do?"
+    }
+}
