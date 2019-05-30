77eeb32375cf415ac762e0e848df6fa4ab3c39e4
Bru <annabru.abh@gmail.com>
2019-05-30 17:55:07 +0200

    Refactored Header.js

--- react-todo-list/src/components/App.js
+++ react-todo-list/src/components/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import ToDoList from "./ToDoList";
-import { getRandomTagline } from "../helpers.js";
+// import { getRandomTagline } from "../helpers.js";
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <Header tagline={getRandomTagline()} />
+      <Header tagline="Here are all the next tasks."/>
+        {/* <Header tagline={getRandomTagline()} /> */}
         <ToDoList />
       </div>
     );
