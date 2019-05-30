fd3c5ac6f6637c67b2fad79c0639846c88106d32
Bru <annabru.abh@gmail.com>
2019-05-30 17:40:53 +0200

    Router works (:

--- react-todo-list/src/components/NotFound.js
+++ react-todo-list/src/components/NotFound.js
@@ -1,21 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import Header from "./Header";
 
-class NotFound extends Component {
+class NotFound extends React.Component {
   componentDidMount() {
-    document.title = "Error 404 - What to do?";
+    document.title = "Error 404 | What to do?";
   }
+
   render() {
     return (
       <div className="container">
-        <Header tagline="404 - Page not found!">
-          <div className="alert alert-warning">
-            <strong>
-              Oops... sorry!<br />
-            </strong>
-            The requested page could not be found.
-          </div>
-        </Header>
+        <Header tagline="404 -  Page not found!" />
+        <div className="alert alert-warning">
+          <strong>
+            Ooops .... sorry!<br />
+          </strong>
+          The requested page could not be found.
+        </div>
       </div>
     );
   }
