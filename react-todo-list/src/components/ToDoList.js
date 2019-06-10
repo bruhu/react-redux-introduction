0d97338281e24d473edd398f8adf0720b5b6f6c7
Bru <annabru.abh@gmail.com>
2019-06-10 14:53:31 +0200

    ToDoItem is now displayed in <table> instead of <ul>

--- react-todo-list/src/components/ToDoList.js
+++ react-todo-list/src/components/ToDoList.js
@@ -5,17 +5,19 @@ class ToDoList extends React.Component {
   render() {
     return (
       <div className="todo-list">
-        <ul className="todo-items">
-          {Object.keys(this.props.items).map(uuid => (
-            <ToDoItem
-              key={`todo-item-${uuid}`}
-              data={this.props.items[uuid]}
-              updateToDoText={this.props.updateToDoText}
-              toggleToDoDone={this.props.toggleToDoDone}
-              removeToDo={this.props.removeToDo}
-            />
-          ))}
-        </ul>
+        <table className="todo-items table table-borderless">
+          <tbody>
+            {Object.keys(this.props.items).map(uuid => (
+              <ToDoItem
+                key={`todo-item-${uuid}`}
+                data={this.props.items[uuid]}
+                updateToDoText={this.props.updateToDoText}
+                toggleToDoDone={this.props.toggleToDoDone}
+                removeToDo={this.props.removeToDo}
+              />
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
